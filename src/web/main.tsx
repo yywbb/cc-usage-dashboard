ff69945d9580bb848b827fcca3e77767e7bbcca6
qice55 <scorliss457@gmail.com>
2026-04-25 02:21:06 +0800

    feat(theme): mount ThemeProvider, register echarts themes on boot

--- src/web/main.tsx
+++ src/web/main.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ConfigProvider } from 'antd';
-import zhCN from 'antd/locale/zh_CN';
 import App from './App.js';
+import { ThemeProvider } from './theme/ThemeProvider.js';
+import { registerEchartsThemes } from './theme/echarts.js';
 
-const qc = new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: true, staleTime: 30_000 } } });
+registerEchartsThemes();
+
+const qc = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: true, staleTime: 30_000 } },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider locale={zhCN}>
+    <ThemeProvider>
       <QueryClientProvider client={qc}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </QueryClientProvider>
-    </ConfigProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
